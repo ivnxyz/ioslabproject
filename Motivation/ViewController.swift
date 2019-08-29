59ba86fb5c0e64fce150e9dacb0921897a3114ea
Iván Martínez <ivanmartinez33@outlook.com>
2019-08-29 16:59:48 -0500

    Initial Commit

---
+++ Motivation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Motivation
+//
+//  Created by Iván Martínez on 8/29/19.
+//  Copyright © 2019 Iván Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
